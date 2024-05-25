cb867c7b5c6d24801f2d90634957ddc723b7e0d0
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2024-05-25 20:57:14 +0530

    Create 121.py

---
+++ 121.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        profit = 0
+        min_price = prices[0]
+        for i in range(1, len(prices)):
+            if(prices[i]<min_price):
+                min_price = prices[i]
+            elif(prices[i]-min_price>profit):
+                profit = prices[i]-min_price
+
+        return profit
+
+# Runtime - 740 ms 
+# Memory - 20.88 MB
