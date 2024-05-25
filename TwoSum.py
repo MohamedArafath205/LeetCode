186cc7341c0c87da0340eb59e0b909752234cad3
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2024-05-25 18:31:14 +0530

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if(j==i):
+                    j+=1
+                else:    
+                    if (nums[i]+nums[j]==target):
+                        return i,j
+
+# Memory - 12.31 MB
+# RunTime - 5301 ms
