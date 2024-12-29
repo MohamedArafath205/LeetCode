a19e5f50dbee12000f6c9495294402d5cfdb58ea
Mohamed Arafath <113768609+MohamedArafath205@users.noreply.github.com>
2024-12-29 23:02:53 +0530

    Create 2703.js

---
+++ 2703.js
@@ -0,0 +1,16 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+/** 
+* Runtime - 40ms
+* Memory - 48.00MB
+*/
